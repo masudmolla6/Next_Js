ebf045877962b4d494dc1654aec4e79c0bad3e84
masudmolla6 <masudmolla2937@gmail.com>
2026-04-14 22:29:15 +0600

    Handle Different layout

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -6,7 +6,9 @@ const DashboardLayout = ({children}) => {
     return (
         <div className='grid grid-cols-12 gap-5 w-full min-h-screen my-4'>
             <div className='col-span-3 border-r-2'>
-                <h2>Navigation</h2>
+                <div className='flex justify-center'>
+                    <Link href="/" className="text-lg text-center w-full">😊 Dev Storey</Link>
+                </div>
                 <div className='flex flex-col gap-6 mt-10'>
                     <Link className='px-4 py-2 bg-gray-700 text-white rounded-2xl mx-4 text-center' href="/dashboard/add-story">Add Story</Link>
                     <Link className='px-4 py-2 bg-gray-700 text-white rounded-2xl mx-4 text-center' href="/dashboard/my-profile">My Profile</Link>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+"use client";
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Header = () => {
+  const pathname=usePathname();
+  console.log(pathname);
+  if(pathname.startsWith("/dashboard")) return <></>;
     return (
         <header className="px-4 py-3 border-b-2 border-b-gray-700 flex justify-between items-center flex-wrap">
           <Link href="/" className="text-lg">😊 Dev Storey</Link>

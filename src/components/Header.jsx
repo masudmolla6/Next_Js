3535e60edd6eec4084c33aed470c484984b28cd8
masudmolla6 <masudmolla2937@gmail.com>
2026-04-15 21:10:01 +0600

    Added loading page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
+import NavLink from './NavLink';
 
 const Header = () => {
   const pathname=usePathname();
@@ -11,12 +12,12 @@ const Header = () => {
         <header className="px-4 py-3 border-b-2 border-b-gray-700 flex justify-between items-center flex-wrap">
           <Link href="/" className="text-lg">😊 Dev Storey</Link>
           <nav className='space-x-2'>
-            <Link href="/">Home</Link>
-            <Link href="/stories">Stories</Link>
-            <Link href="/about">About</Link>
-            <Link href="/tutorials">Tutorials</Link>
-            <Link href="/login">Login</Link>
-            <Link href="/register">register</Link>
+            <NavLink href="/">Home</NavLink>
+            <NavLink href="/stories">Stories</NavLink>
+            <NavLink href="/about">About</NavLink>
+            <NavLink href="/tutorials">Tutorials</NavLink>
+            <NavLink href="/login">Login</NavLink>
+            <NavLink href="/register">register</NavLink>
           </nav>
         </header>
     );
